0d7f342433890a2c33831f521fe03da6fd88b43b
Cornapple <1031995@student.sae.edu.au>
2026-06-01 16:06:08 +1000

    insert ground fluid assets and edit for implementation

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Health Settings")]
+    public int playerBaseHealth;
+    public int playerHealth;
+    public int playerMaxHealth;
+
+    [Header("Level Settings")]
+    public int playerBaseLevel;
+    public int playerLevel;
+    public int playerMaxLevel;
+
+    [Header("Damage Settings")]
+    public int playerBaseDamage;
+    public int playerDamage;
+    public int playerMaxDamage;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
